d32ba2d726926dcb8cd5ce9d6f53e01f098afde0
Himadri Bera <your-email@example.com>
2026-04-08 17:34:10 +0530

    Added digital clock project

---
+++ script.js
@@ -0,0 +1,16 @@
+// let a=new Date
+// // console.log(a.getHours())
+// console.log(a.getMinutes())
+
+
+let hours=document.getElementById("hrs");
+let minutes=document.getElementById("mins");
+let seconds=document.getElementById("sec");
+let date=document.getElementById("dat");
+let month=document.getElementById("mon");
+let day=document.getElementById("day");
+let year=document.getElementById("year");
+
+
+let time =new  Date 
+hours.innerHTML=time.getHours()
